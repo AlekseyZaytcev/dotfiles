f3bcea142a2ae3d5ff45a27a3946b9248f11e442
Aleksey Zaitsev <aleksey2142.zaytcev@gmail.com>
2024-04-20 20:49:41 +0300

    Add Wezterm configuration

---
+++ .wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+-- DoomOne
+-- Hybrid	- for work in dark
+-- FirefoxDev	- for work in really dark
+-- Tomorrow	- for work in light
+-- GruvboxDark
+-- Mariana
+-- Dark+
+-- Dracula / +
+-- Molokai
+-- Calamity	- smth interesting
+-- Retro	- green hell
+config.color_scheme = 'DoomOne'
+
+return config
